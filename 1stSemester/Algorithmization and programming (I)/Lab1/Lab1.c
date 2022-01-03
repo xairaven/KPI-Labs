693dcca63f7529d4e44a8d09fb10237ff19a5ca0
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-04 00:47:31 +0200

    Lab1.c updated

--- 1stSemester/Algorithmization and programming (I)/Lab1/Lab1.c
+++ 1stSemester/Algorithmization and programming (I)/Lab1/Lab1.c
@@ -1,9 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#define ln(x) log(x)
+double formula (double a, double b, double x);
 
-int main(void) {
+int main(int argc, char *argv[]) {
   double a, b, x, y;
-  printf("Input a, b, x\n");
-  scanf("%lf%lf%lf", &a, &b, &x);
+  int choice = 0;
+  printf("1. Args from main\n2. Args from scan\n3. Args in code\n");
+  scanf("%d", &choice);
+  switch (choice) {
+    case 1:
+      if (argc == 4) {
+        y = formula (atof(argv[1]), atof(argv[2]), atof(argv[3]));
+        if (y != 0) {
+          printf("y = %lf\n", y);
+        } else {
+          printf("Не відповідає ОДЗ\n");
+        }
+      } else {
+        printf ("Аргументів немає\n");
+        break;
+      }
+      break;
+    case 2:
+      printf("Введіть числа\n");
+      scanf("\n%lf%lf%lf", &a, &b, &x);
+      y = formula (a, b, x);
+      if (y != 0) {
+        printf("y = %lf\n", y);
+      } else {
+        printf("Не відповідає ОДЗ\n");
+      }
+      break;
+    case 3:
+      a = 1; b = 2; x = 3; y = formula (a, b, x);
+      if (y != 0) {
+        printf("y = %lf\n", y);
+      } else {
+        printf("Не відповідає ОДЗ\n");
+      }
+      break;
+    default:
+      printf("Такого числа в списку немає\n");
+      break;
+  }
+}
+
+double formula (double a, double b, double x) {
+  if (a != 0 && x != 0) {
+    return log(a*x) + x + cos(x) + b*x + pow(a, b);
+  } else {
+    return 0;
+  }
 }
