0e67c0d8436d517c345e81f87bbdb8373bc159b9
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-08 21:10:40 +0200

    initial commit

---
+++ 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/MCW1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int matrix[3][3] = {
+            {2, 1, 2},
+            {3, 2, 1},
+            {2, 2, 2}
+    };
+    return 0;
+}
