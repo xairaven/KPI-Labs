8e0197ce3d441abb30465ffa96965a734553c577
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-09 22:13:13 +0200

    final update

--- 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.h
+++ 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.h
@@ -6,9 +6,17 @@
 #define MCW1_XAIMATRIX_H
 #include <stdio.h>
 #include <stdlib.h>
-int** declareMatrix(int SIZE); //creating dynamic array
-void initialisingMatrix(int** matrix, const int* rawMatrix, int SIZE);//static array -> dynamic array
-int** getQuadMinor(int** matrix, int h, int v, int matrixSize); //get matrix minor
-void printMatrix(int** matrix, int SIZE); //print matrix
 int getDet(int** matrix, int SIZE);
+
+int** declareMatrix(int SIZE); //creating dynamic array
+int** getQuadMinor(int** matrix, int h, int v, int matrixSize); //get matrix minor
+int** matrixTranspose(int** matrix, int SIZE);
+
+float** declareFloatMatrix(int SIZE);
+float** inverseMatrix(int** matrix, int SIZE);
+float** matrixFloatTranspose(float** matrix, int SIZE);
+
+void initialisingMatrix(int** matrix, const int* rawMatrix, int SIZE);//static array -> dynamic array
+void printMatrix(int** matrix, int SIZE); //print matrix
+void printFloatMatrix(float** matrix, int SIZE);
 #endif //MCW1_XAIMATRIX_H
