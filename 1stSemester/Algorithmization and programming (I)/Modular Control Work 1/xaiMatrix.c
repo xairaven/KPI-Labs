175a0a41fcc4a8c279eea5c0e687f335053a6c53
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-09 19:59:52 +0200

    update xaiMatrix.c
    added getDet() function

--- 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.c
+++ 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.c
@@ -2,12 +2,12 @@
 // Created by Alex on 09.01.2022.
 //
 #include "xaiMatrix.h"
-int** getQuadMinor(int** matrix, int h, int v, int* minorSIZE) {
-    *minorSIZE -= 1;
-    int** newData = declareMatrix((*minorSIZE) - 1);
-    for (int i = 0; i < *minorSIZE; i++) {
+
+int** getQuadMinor(int** matrix, int h, int v, int minorSize) {
+    int** newData = declareMatrix(minorSize);
+    for (int i = 0; i < minorSize; i++) {
         if (i < h - 1) {
-            for (int j = 0; j < *minorSIZE; j++) {
+            for (int j = 0; j < minorSize; j++) {
                 if (j < v - 1) {
                     newData[i][j] = matrix[i][j];
                 } else {
@@ -15,7 +15,7 @@ int** getQuadMinor(int** matrix, int h, int v, int* minorSIZE) {
                 }
             }
         } else {
-            for (int j = 0; j < *minorSIZE; j++) {
+            for (int j = 0; j < minorSize; j++) {
                 if (j < v - 1) {
                     newData[i][j] = matrix[i + 1][j];
                 } else {
@@ -51,4 +51,23 @@ void initialisingMatrix(int** matrix, const int* rawMatrix, int SIZE) {
             matrix[i][j] = rawMatrix[k];
         }
     }
+}
+
+int getDet(int** matrix, int SIZE) {
+    if (SIZE == 2) {
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+    }
+    int total = 0;
+    int num = SIZE;
+    int* nums = calloc(num, sizeof(int));
+    for (int i = 0; i < num; i++) {
+        nums[i] = matrix[0][i] * getDet(getQuadMinor(matrix, 1, i + 1, SIZE - 1), SIZE - 1);
+        if(i % 2 != 0) {
+            nums[i] *= -1;
+        }
+    }
+    for (int i = 0; i < num; i++) {
+        total += nums[i];
+    }
+    return total;
 }
