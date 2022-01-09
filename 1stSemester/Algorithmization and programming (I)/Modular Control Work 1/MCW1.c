eb6d96b49ccfc1536979ac49619503e96d94688c
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-09 15:23:15 +0200

    library update

--- 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/MCW1.c
+++ 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/MCW1.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-int** declareMatrix(int SIZE); //creating dynamic array
-void initialisingMatrix(int** matrix, int* rawMatrix, int SIZE);//static array -> dynamic array
-int** getQuadMinor(int** matrix, int h, int v, int* minorSIZE);//get matrix minor
-void printMatrix(int** matrix, int SIZE); //print matrix
-void cleanMatrix(int** matrix, int SIZE); //cleaning memory
+#include "xaiMatrix.h"
 
 #define ORIG_SIZE 3 //dimensions of array. USER INPUT
 int main(void) {
@@ -18,67 +12,6 @@ int main(void) {
     int minorSIZE = SIZE;
     int** matrix = declareMatrix(SIZE);
     initialisingMatrix(matrix, (int*) rawMatrix, SIZE);
-
     int** minor = getQuadMinor(matrix, 2, 1, &minorSIZE);
     printMatrix(minor, minorSIZE);
-    cleanMatrix(matrix, SIZE);
-    cleanMatrix(minor, minorSIZE);
-}
-
-int** getQuadMinor(int** matrix, int h, int v, int* minorSIZE) {
-    *minorSIZE -= 1;
-    int** newData = declareMatrix((*minorSIZE) - 1);
-    for (int i = 0; i < *minorSIZE; i++) {
-        if (i < h - 1) {
-            for (int j = 0; j < *minorSIZE; j++) {
-                if (j < v - 1) {
-                    newData[i][j] = matrix[i][j];
-                } else {
-                    newData[i][j] = matrix[i][j + 1];
-                }
-            }
-        } else {
-            for (int j = 0; j < *minorSIZE; j++) {
-                if (j < v - 1) {
-                    newData[i][j] = matrix[i + 1][j];
-                } else {
-                    newData[i][j] = matrix[i + 1][j + 1];
-                }
-            }
-        }
-    }
-    return newData;
-}
-
-void printMatrix(int** matrix, int SIZE) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            printf("%d\t", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int** declareMatrix(int SIZE) {
-    int** matrix = (int **) malloc(SIZE * sizeof(int *));
-    for(int i = 0; i < SIZE; i++) {
-        matrix[i] = (int *) malloc(SIZE * sizeof(int));
-    }
-    return matrix;
-}
-
-void initialisingMatrix(int** matrix, int* rawMatrix, int SIZE) {
-    int k = 0;
-    for(int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++, k++) {
-            matrix[i][j] = rawMatrix[k];
-        }
-    }
-}
-
-void cleanMatrix(int** matrix, int SIZE) {
-    for(int i = 0; i < SIZE; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
 }
