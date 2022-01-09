1a336659aefc8b05028985ce49c769c87cc68921
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-09 23:03:23 +0200

    added matrix multiplication

--- 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.h
+++ 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.h
@@ -6,15 +6,22 @@
 #define MCW1_XAIMATRIX_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+bool legalMultiplicationOperation(int SIZE1, int SIZE2);
+
 int getDet(int** matrix, int SIZE);
 
 int** declareMatrix(int SIZE); //creating dynamic array
 int** getQuadMinor(int** matrix, int h, int v, int matrixSize); //get matrix minor
 int** matrixTranspose(int** matrix, int SIZE);
 
+float multRowCol (int** matrix1, float** matrix2, int SIZE1, int SIZE2, int row, int col);
+
 float** declareFloatMatrix(int SIZE);
 float** inverseMatrix(int** matrix, int SIZE);
 float** matrixFloatTranspose(float** matrix, int SIZE);
+float** multMat (int** matrix1, float** matrix2, int SIZE1, int SIZE2);
 
 void initialisingMatrix(int** matrix, const int* rawMatrix, int SIZE);//static array -> dynamic array
 void printMatrix(int** matrix, int SIZE); //print matrix
