de04aa32ef7a3f6d711389be5b44ffae2f65c57b
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-09 20:00:33 +0200

    update xaiMatrix.h
    added prototype of getDet() function.

--- 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.h
+++ 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/xaiMatrix.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 int** declareMatrix(int SIZE); //creating dynamic array
 void initialisingMatrix(int** matrix, const int* rawMatrix, int SIZE);//static array -> dynamic array
-int** getQuadMinor(int** matrix, int h, int v, int* minorSIZE);//get matrix minor
+int** getQuadMinor(int** matrix, int h, int v, int matrixSize); //get matrix minor
 void printMatrix(int** matrix, int SIZE); //print matrix
+int getDet(int** matrix, int SIZE);
 #endif //MCW1_XAIMATRIX_H
