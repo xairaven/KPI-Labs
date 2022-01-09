fa86e3a28ff00bd8090c2bd5629de5dd3186b69c
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-01-09 19:59:03 +0200

    added calculating determinant || MCW1.c
    now you can calculate determinant of every quadratic matrix

--- 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/MCW1.c
+++ 1stSemester/Algorithmization and programming (I)/Modular Control Work 1/MCW1.c
@@ -1,17 +1,18 @@
 #include "xaiMatrix.h"
 
-#define ORIG_SIZE 3 //dimensions of array. USER INPUT
+#define ORIG_SIZE 4 //dimensions of array. USER INPUT
 int main(void) {
     int rawMatrix[ORIG_SIZE][ORIG_SIZE] = { //Matrix. USER INPUT
-            {2, 1, 2},
-            {3, 2, 1},
-            {2, 2, 2}
+            {2, 4, 1, 1},
+            {0, 2, 1, 0},
+            {2, 1, 1, 3},
+            {4, 0, 2, 3},
     };
 
     int SIZE = ORIG_SIZE;
-    int minorSIZE = SIZE;
     int** matrix = declareMatrix(SIZE);
     initialisingMatrix(matrix, (int*) rawMatrix, SIZE);
-    int** minor = getQuadMinor(matrix, 2, 1, &minorSIZE);
-    printMatrix(minor, minorSIZE);
+
+    int det = getDet(matrix, SIZE);
+    printf("Determinant = %d\n", det);
 }
