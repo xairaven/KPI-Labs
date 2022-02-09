aeb91de931bdb96191a9383fdd62673b5edcdb01
Alex Kovalyov <86195722+xairaven@users.noreply.github.com>
2022-02-09 23:02:30 +0200

    update: fixed lab1.asm

--- 2ndSemester/Operating Systems/Lab1/Lab1.asm
+++ 2ndSemester/Operating Systems/Lab1/Lab1.asm
@@ -9,10 +9,9 @@ i1 db -255         ; must be WORD value
 ii db -128, 127
 
 ; my values (byte)
-i2 db -115
-i3 db 115
-i4 db -157         ; must be WORD value
-i5 db 157
+   db -115
+   db 115
+   db 157
 
 ; word
 r0 dw -255         
@@ -23,9 +22,10 @@ r3 dw -65535        ; must be DOUBLE WORD value
 r4 dd -65535
 
 ; my values (word)
-r5 dw -1704
-r6 dw 1704
-r7 dw -1746
-r8 dw 1746
+   dw -157
+   dw -1704
+   dw 1704
+   dw -1746
+   dw 1746
 
 end
