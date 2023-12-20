bb6d40d9a054bce7487c51a82498e50d6ad1916b
xairaven <alexkovalevkpi@gmail.com>
2023-12-20 18:01:31 +0200

    SWDT: Lab 8 done

---
+++ 5thSemester/SW-Development-Technologies/Lab8/src/DAL/Entities/Shipment.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Enums;
+
+namespace DAL.Entities;
+
+public class Shipment
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public ShipmentStatus Status { get; set; }
+    public int DriverId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
---
+++ 5thSemester/SW-Development-Technologies/Lab8/src/DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Repositories.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    T Get(int id);
+    IEnumerable<T> Find(Func<T, bool> predicate, int pageNumber = 0, int pageSize = 10);
+    void Create(T item);
+    void Update(T item);
+    void Delete(int id);
+}
