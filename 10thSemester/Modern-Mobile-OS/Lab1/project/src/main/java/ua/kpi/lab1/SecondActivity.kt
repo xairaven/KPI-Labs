a9941d55463bfe38215d81bd1cdac3988db01f48
xairaven <alexkovalevkpi@gmail.com>
2026-02-19 01:02:54 +0200

    MMOS: Lab 1 done

---
+++ 10thSemester/Modern-Mobile-OS/Lab1/project/src/main/java/ua/kpi/lab1/SecondActivity.kt
@@ -0,0 +1,25 @@
+package ua.kpi.lab1
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val surname = intent.getStringExtra("SURNAME_KEY")
+        tvResult.text = "Параметр: $surname"
+    }
+}
